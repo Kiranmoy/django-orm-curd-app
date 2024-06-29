5e6a03d2dc5a8c097691fd69788269cf1e0dd37b
Kiranmoy <paul.kiranmoy@gmail.com>
2024-06-30 02:19:35 +0530

    Hands-on Lab: CRUD on Django Model Objects

---
+++ crud/read_courses.py
@@ -0,0 +1,15 @@
+# Django specific settings
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+# Ensure settings are read
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
+
+from crud.models import *
+
+# Your code starts from here:
+# Find all courses
+courses = Course.objects.all()
+print(courses)
